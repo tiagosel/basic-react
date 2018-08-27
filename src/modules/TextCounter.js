62a998e6dc136aa00a3e5b9d85e842bc9fb9e68f
Tiago Neves Borges <tiago.borges@oi.net.br>
2018-08-27 12:42:44 -0300

    .

---
+++ src/modules/TextCounter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class TextCounter extends React.Component{
+
+    render(){
+        return(
+    
+        <h1>Teste Tiago</h1>
+        )
+
+    }
+
+
+
+
+}
+
+
+export default TextCounter
