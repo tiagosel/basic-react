988b67c0bf3f01db3cc2be8fe31d56631ac98db2
Tiago Neves Borges <tiago.borges@oi.net.br>
2018-08-27 13:37:26 -0300

    .

--- src/modules/TextCounter.js
+++ src/modules/TextCounter.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 
 class TextCounter extends React.Component{
+    static defaulProps = {
+        lable:''
+    }
+ 
+    
+    
+    state = {
+        totalChars:0
+    }
+ 
+    countCharacters(event){
+        var element = event.target;
+        var text = element.value;
+        this.setState({totalChars:text.length});
+    }
+
+
 
     render(){
         return(
+            <div>
     
-        <h1>Teste Tiago</h1>
+                <textarea onChange={this.countCharacters.bind(this)}/>
+                <div>
+                    <span>{this.props.lable}:{this.state.totalChars}</span>
+                </div>
+            </div>
         )
 
     }
